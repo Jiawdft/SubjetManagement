334450088827394915365bf5c52bd9598fb6bd9b
Jiawei <jiawei@hotmail.fr>
2018-06-01 22:35:55 +0200

    Entities

--- src/fr/isep/core/teacher/ListSubjects.java
+++ src/fr/isep/core/teacher/ListSubjects.java
@@ -1,4 +1,22 @@
 package fr.isep.core.teacher;
 
+import java.util.TreeMap;
+
 public class ListSubjects {
+    private TreeMap<String, Subject> subjectTreeMap;
+
+    public ListSubjects() {
+    }
+
+    public ListSubjects(TreeMap<String, Subject> subjectTreeMap) {
+        this.subjectTreeMap = subjectTreeMap;
+    }
+
+    public TreeMap<String, Subject> getSubjectTreeMap() {
+        return subjectTreeMap;
+    }
+
+    public void setSubjectTreeMap(TreeMap<String, Subject> subjectTreeMap) {
+        this.subjectTreeMap = subjectTreeMap;
+    }
 }
